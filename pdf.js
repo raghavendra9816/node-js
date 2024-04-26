25e080b615174f2e56f8b779c441c15e3e4dd053
raghavendra9816 <167013421+raghavendra9816@users.noreply.github.com>
2024-04-26 13:45:13 +0545

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,32 +1,27 @@
 
-const http=require("http");
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
+const http = require("http");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
 
 http
-  .createServer(async (req, res) => {
-
-    console.log(req.url)
+  .createServer((req, res) => {
+    const url = req.url.split("=");
+    const text = url[1];
     if (req.url.split("=")[1] !== undefined) {
-      const qr = await QRCode.toDataURL(req.url.split("=")[1]);
-      const imgsrc = `<img src="${qr}"/>`;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("pdf generated");
+      createpdf(text);
     }
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end("pdf generated");
   })
   .listen(8000);
 console.log("running on port 8000");
 
+const createpdf = (userinput) => {
+  //create document
+  const doc = new PDFDocument();
 
-
-
-
-
-const createpdf=(userinput)=>{
-
-}
-const doc = new PDFDocument();
-doc
-  .font('fonts/PalatinoBold.ttf')
-  .fontSize(25)
-  .text('Some text with an embedded font!', 100, 100);
+  doc.pipe(fs.createWriteStream("output.pdf"));
+  doc.text(userinput, 100, 100);
+  doc.end();
+};
